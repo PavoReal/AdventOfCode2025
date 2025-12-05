29a5fc3bf6924767c1b40f390f2c96da7bd4682a
Garrison Peacock <garrison.peacock@gmail.com>
2025-12-04 17:48:10 -0800

    Day 04 part 1

--- src/day_04.zig
+++ src/day_04.zig
@@ -6,26 +6,19 @@ const DayFour = struct {
 
     grid_width: usize = 0,
     grid_height: usize = 0,
-
     grid_stride: usize = 0,
 
     grid: []const u8 = undefined,
 
-    fn areCoordsValud(self: *DayFour, x: usize, y: usize) bool {
-        return (x < self.grid_width) and (y < self.grid_height);
-    }
-
     fn countNeighbors(self: *DayFour, x: usize, y: usize) u8 {
         var result: u8 = 0;
 
-        if (!areCoordsValud(self, x, y)) return 0;
+        if ((x >= self.grid_width) or (y >= self.grid_height)) return 0;
 
         const min_x = @as(usize, @intCast(@max(0, @as(i32, @intCast(x)) - 1)));
-        const max_x = @as(usize, @intCast(@min(self.grid_width, @as(i32, @intCast(x)) + 1)));
+        const max_x = @as(usize, @intCast(@min(self.grid_width, @as(i32, @intCast(x)) + 2)));
         const min_y = @as(usize, @intCast(@max(0, @as(i32, @intCast(y)) - 1)));
-        const max_y = @as(usize, @intCast(@min(self.grid_height, @as(i32, @intCast(y)) + 1)));
-
-        std.log.debug("min x {} max x {} min y {} max y {}", .{ min_x, max_x, min_y, max_y });
+        const max_y = @as(usize, @intCast(@min(self.grid_height, @as(i32, @intCast(y)) + 2)));
 
         for (min_y..max_y) |yy| {
             for (min_x..max_x) |xx| {
@@ -44,44 +37,41 @@ const DayFour = struct {
 
         return result;
     }
-
-    pub fn dayFour() DayFour {
-        var self: DayFour = .{};
-
-        const input_path = "./inputs/day_four.txt";
-        self.grid = @embedFile(input_path);
-
-        const stride_ = std.mem.indexOfScalar(u8, self.grid, '\n');
-
-        if (stride_ == null) {
-            std.log.err("Couldn't parse file stride", .{});
-            return .{};
-        }
-
-        self.grid_stride = stride_.? + 1;
-        self.grid_width = stride_.?;
-        self.grid_height = (self.grid.len / self.grid_stride) - 1;
-
-        self.part_one = 0;
-
-        for (0..self.grid_height) |y| {
-            for (0..self.grid_width) |x| {
-                const neighbors = self.countNeighbors(x, y);
-
-                std.log.debug("x {} y {} n {}", .{ x, y, neighbors });
-
-                if (neighbors < 4) {
-                    self.part_one += 1;
-                }
-            }
-        }
-
-        return self;
-    }
 };
 
 pub fn main() void {
-    const day: DayFour = DayFour.dayFour();
+    var day: DayFour = .{};
+
+    const input = @embedFile("./inputs/day_four.txt");
+    day.grid = input;
+
+    const width = std.mem.indexOfScalar(u8, day.grid, '\n');
+
+    if (width == null) {
+        std.log.err("Couldn't parse file stride", .{});
+        return;
+    }
+
+    day.grid_width = width.?;
+    day.grid_stride = day.grid_width + 1;
+    day.grid_height = std.mem.countScalar(u8, day.grid, '\n');
+    day.part_one = 0;
+
+    for (0..day.grid_height) |y| {
+        for (0..day.grid_width) |x| {
+            const spot = day.grid[(y * day.grid_stride) + x];
+
+            if (spot != '@') {
+                continue;
+            }
+
+            const neighbors = day.countNeighbors(x, y);
+
+            if (neighbors < 4) {
+                day.part_one += 1;
+            }
+        }
+    }
 
     std.log.info("Day 4 part 1: {d}", .{day.part_one});
     std.log.info("Day 4 part 2: {d}", .{day.part_two});
